080683a7d23392d820618cf0e43e619618513acd
arale025 <yoyahihello@yahoo.com.tw>
2023-10-11 00:01:46 +0800

    feat: RWD homepage, tos, privacy policy

---
+++ js/all.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+  // 按漢堡打開選單
+  $('.burger').click(function () {
+    $('body').addClass('open');
+    $('header').addClass('blur');
+    $('section').addClass('blur');
+    $('footer').addClass('blur');
+    $('.menu::after').addClass('closeafter');
+  });
+  // 按Ｘ關閉選單
+  $('.close').click(function (event) {
+    $('body').removeClass('open');
+    $('header').removeClass('blur');
+    $('section').removeClass('blur');
+    $('footer').removeClass('blur');
+  });
+
+  // 按旁邊畫面關掉選單
+
+  $('section').click(function (event) {
+    $('body').removeClass('open');
+    $('header').removeClass('blur');
+    $('section').removeClass('blur');
+    $('footer').removeClass('blur');
+  });
+
+  $('footer').click(function (event) {
+    $('body').removeClass('open');
+    $('header').removeClass('blur');
+    $('section').removeClass('blur');
+    $('footer').removeClass('blur');
+  });
+});
